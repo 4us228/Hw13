8e0bb34accb8db03e72a960d618125b09a2fc0f9
4us228 <4us.in.ua@gmail.com>
2021-08-09 18:01:54 +0300

    12

--- src/Kris/start/Money/step/Main.java
+++ src/Kris/start/Money/step/Main.java
@@ -1,12 +1,44 @@
 package Kris.start.Money.step;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
 
+
+    public static final int CARS_COUNT = 4;
 
     public static void main(String[] args) {
 
 
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT + 1);
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10),cb);
+            new Thread(cars[i]).start();
+        }
+
+        try {
+            cb.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            cb.await();
+            cb.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
+
 
 
